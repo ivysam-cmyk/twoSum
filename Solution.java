4197dbaa3e92e9ae622728dbd34afd438c7c72e1
sam <thambad@gmail.com>
2023-08-25 21:49:43 +0800

    working 55/60 test cases

---
+++ Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        //there are 2 nums to use inside the array.
+        //take 1st element and use the next elements and sum until you get the answer
+        int[] twoNums = new int[2];
+        for(int i =0; i<nums.length-1; i++){
+            for(int j = i+1; j>i && j<nums.length; j++){
+                System.out.println("i: "+ i + " j: "+ j);
+                System.out.println(nums[i]+ nums[j]);
+                if(nums[i] + nums[j] == target){
+                    twoNums[0] = i;
+                    twoNums[1] = j;
+                    return twoNums;
+                }
+            }
+        }
+        return twoNums;
+    }
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] arr = {3,2,4};
+        s.twoSum(arr, 6);
+    }
+}
