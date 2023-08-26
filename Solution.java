c94d2de7a6b6b213b440faff5fae0b15697bdcba
sam <thambad@gmail.com>
2023-08-26 12:54:09 +0800

    solution accepted

--- Solution.java
+++ Solution.java
@@ -1,19 +1,30 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        //there are 2 nums to use inside the array.
-        //take 1st element and use the next elements and sum until you get the answer
         int[] twoNums = new int[2];
         for(int i =0; i<nums.length-1; i++){
             int reqVal = target - nums[i];
             System.out.println("reqVal: "+reqVal);
-            for (int j=i+1;j>i && j<nums.length; j++) {
-                System.out.println("i: " + i + " j: " + j);
-                if (nums[j]==reqVal){
-                    twoNums[0] = i;
-                    twoNums[1] = j;
-                    return twoNums;
-                }
+            List<Integer> numsList =  Arrays.stream(nums).boxed().collect(Collectors.toList());
+            //slice
+            numsList = numsList.subList(i+1,nums.length);
+            // System.out.println("numsList: "+numsList);
+            int reqIndex;
+            //if the reqVal isn't in numsList skip this value of i
+            if(numsList.indexOf(reqVal) == -1){
+                continue;
+            } else{
+                // i+1 accounts for the slicing above
+                reqIndex = numsList.indexOf(reqVal) +(i+1);
             }
+            System.out.println("i: "+i);
+            System.out.println("reqIndex: "+reqIndex);
+            twoNums[0] = i;
+            twoNums[1] = reqIndex;
+            return twoNums;
         }
         return twoNums;
     }
