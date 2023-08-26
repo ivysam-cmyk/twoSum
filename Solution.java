b6ca82a953d19dba093498b9ea567522b1e80014
sam <thambad@gmail.com>
2023-08-26 15:23:32 +0800

    working with 45/60

--- Solution.java
+++ Solution.java
@@ -12,9 +12,19 @@ class Solution {
         }
         //once map is filled up go through the nums arr again and check the if the key is present in the other indexes
         //to do this: for each value(index) in the map, check all the other indexes in the nums arr whether it == key
-        for (int i = 0; i < nums.length; i++) {
-
+        System.out.println(complementMap);
+        for(int complement : complementMap.keySet()){
+            System.out.println("complement: "+complement);
+            for (int j =0; j<nums.length;j++) {
+                // make sure that complement == number in nums and it is not the same index as 3+3 =6
+                System.out.println(complementMap.get(complement) + " " + j);
+                if(nums[j]== complement && j!= complementMap.get(complement) ){
+                    System.out.println(complementMap.get(complement) + " " + j);
+                    return new int[] {complementMap.get(complement), j};
+                }
+            }
         }
+        return new int[]{};
     }
     public static void main(String[] args) {
         Solution s = new Solution();
